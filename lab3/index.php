94a29c61a2d14e03f5ef5725a5c62c9358e23188
vovasar <wowasar@icloud.com>
2026-01-16 12:51:56 +0300

    lab3 1

--- lab3/index.php
+++ lab3/index.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="text" id="">
+        <button type="submit">but</button>
+    </form>
+
+</body>
+</html>
 <?php
-echo 'egortupoy'
-?>
+if(isset($_GET['text'])){   
+$massive_slov = explode(' ',$_GET['text']);
+upFunc($massive_slov);
+echo implode(' ',$massive_slov);
+  
+       }
+
+function upFunc(&$massive_slov){
+for($i=0; $i< count($massive_slov); $i++){
+if(($i % 2)>0){
+    $massive_slov [$i]=strtoupper($massive_slov[$i]);    
+
+}
+}
+//var_dump($massive_slov);
+}
